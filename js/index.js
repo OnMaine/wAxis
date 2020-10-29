30b3a282989c9d49d7c6fc5d0f24ec72e547b391
Володимир <onmaine@gmail.com>
2020-10-29 19:20:50 +0300

    add carousel

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// select
+$(function() {
+  $('select, .select').selectric();
+});
+
+// Owl Carousel
+var $homeSlider = $(".home-slider");
+
+$(window).resize(function() {
+  showHomeSlider();
+});
+
+function showHomeSlider() {
+  if ($homeSlider.data("owlCarousel") !== "undefined") {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      initialHomeSlider();
+    } else {
+      destroyHomeSlider();
+    }
+  }
+}
+showHomeSlider();
+
+function initialHomeSlider() {
+  $homeSlider.addClass("owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: false,
+    dots: true,
+    dotsContainer: '#customDots',
+  });
+}
+
+function destroyHomeSlider() {
+  $homeSlider.trigger("destroy.owl.carousel").removeClass("owl-carousel");
+}
